726102797cac512f0e0ba323d746299e0844989e
Layninou <theophile.guilbaud@ensc.fr>
2016-09-19 01:08:49 +0200

    does not work but tired

---
+++ mod1_solution/app.js
@@ -0,0 +1,24 @@
+(function (){
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope', '$filter'];
+function LunchCheckController($scope, $filter) {
+  $scope.msg = "Check If Too Much";
+  $scope.items = "";
+
+  $scope.checkApp = function () {
+      if ($scope.items = ""){
+        $scope.msg = "Please enter data first";
+      }
+      else{
+        var itemsList = $scope.items.split(",");
+        if (itemsList.length <= 3){$scope.msg = "Enjoy!";}
+        else {$scope.msg ="Too Much!";}
+      }
+  };
+};
+
+})();
